10570459ce64bc6cf00bc846b398f2e351bd1275
Norberto Luna <betoluna89@gmail.com>
2023-03-19 12:00:21 -0300

    Pequeños cambios

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const interes = ((78 / 365) * 30).toFixed(2);
 const sueldo = document.querySelector("#sueldo");
 const meses = document.querySelector("#meses");
 const cuenta = document.querySelector("#cuenta");
-
 const div = document.querySelector(".resultado");
 const btn = document.querySelector(".btn");
+
+const interes = ((78 / 365) * 30).toFixed(2);
 
 btn.addEventListener("click", (e) => {
   let inputSueldo = parseInt(sueldo.value);
@@ -17,18 +17,16 @@ btn.addEventListener("click", (e) => {
     div.innerHTML = `<h2>El rédito en un mes es $${redito}</h2>`;
   } 
   else if (inputMes > 1) {
-    let redito = 0;
     for (let i = 1; i <= inputMes; i++) {
       redito =  reditoUnMes(inputSueldo, interes, inputCuenta)
       acumulado = inputCuenta + redito + inputSueldo;
       inputCuenta = acumulado;
+      div.innerHTML = `<h2>En ${inputMes} meses, invirtiendo $${inputSueldo} por mes, más lo que genera el plazo fijo. Tendrías $${acumulado.toFixed()}. Esa cantidad genera $${redito.toFixed()}.</h2>`;
     }
-    div.innerHTML = `<h2>En ${inputMes} meses, invirtiendo $${inputSueldo} por mes, más lo que genera el plazo fijo. Tendrías $${acumulado.toFixed()}. Esa cantidad genera $${redito.toFixed()}.</h2>`;
   } 
   else {
     alert("Error en la cantidad de meses");
   }
-
   e.preventDefault();
 });
 
